1e216430b909fdfa5a9565deae2a3f0db90502e6
Li Xie <li_xie1992@hotmail.com>
2019-09-24 14:18:07 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -22,13 +22,13 @@ def play(songs)
   puts("Please enter a song name or number:")
   user_input = gets.strip
   
-  if songs.include?(user_input) 
-    puts("Playing #{user_input}")
-  elsif user_input > 0 && user_input < songs.length + 1
-    puts("Playing #{songs[user_input+1}")
-  else 
-    puts("Invalid input, please try again")
-  end
+  # if songs.include?(user_input) 
+  #   puts("Playing #{user_input}")
+  # elsif user_input > 0 && user_input < songs.length + 1
+  #   puts("Playing #{songs[user_input+1}")
+  # else 
+  #   puts("Invalid input, please try again")
+  # end
 end 
 
 def list(songs)
