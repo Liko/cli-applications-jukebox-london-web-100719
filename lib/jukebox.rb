1f3b08f40d56619509237114163508e7aded5751
Li Xie <li_xie1992@hotmail.com>
2019-09-24 14:01:09 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,8 +1,15 @@
-def say_hello(name)
-  "Hi #{name}!"
+def help 
+  
+end 
+
+def play 
+  
+end 
+
+def list 
+  
+end 
+
+def exit_jukebox
+  
 end
- 
-puts "Enter your name:"
-users_name = gets.strip
- 
-puts say_hello(users_name)
